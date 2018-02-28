34693603dabaad1929d0f09576379c0dbcfc44b9
Yoshi Carberry <phineascarb@gmail.com>
2018-02-28 14:11:15 -0600

    You know what dummy

---
+++ TAKB/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+/*phins cheat sheat
+ one = means assign, two == means check if equal to each other
+*/
+
+namespace TAKB {
+	
+	class MainClass {
+		
+		public static void Main (string[] args) {
+
+//=========================INT Block============================\\
+
+			//Multiplication Integers 
+			int num01;
+			int num02;
+			int num03;
+
+//=========================STRING Block==========================\\
+
+//=========================Stuff n things========================\\
+
+
+			Console.WriteLine ("I'm the all knowing-ish bot\npress space to begin your journey");
+			Console.ReadKey ();
+
+			Console.WriteLine ("\nLets exploire my mathmatical capabilities");
+			Console.Write ("First some good ol multiplying (gimme a number):");
+
+			num01 = Convert.ToInt32 (Console.ReadLine());
+
+			Console.Write ("Well I need a second one dummy: ");
+
+			num02 = Convert.ToInt32 (Console.ReadLine());
+			num03 = num01 * num02;
+
+			Console.WriteLine ("\nYour number is " + num01*num02*num03);
+			Console.Write ("\nbam arn't I so shmart?");
+			Console.Write ("\nYour answer: ");
+
+			//Get the answer from the user as an answer object
+			Answer userReturn = new Answer (Console.ReadLine ()); 
+
+			if (userReturn.isYes ()) {
+				Console.WriteLine ("Division!");
+				Console.ReadKey ();
+			} 
+			else if (userReturn.isUnknown()) {
+				Console.WriteLine ("Ugh your answer is not preprogrammed in my database,\nI'm not thaaat smart yet ok gimme some slack\nLets just move on to division!");
+				Console.ReadKey ();
+			}
+			else {
+					Console.WriteLine ("Oh really? I guess ill work on that\nMoving on to division!");
+					Console.ReadKey ();
+				
+			}
+
+		}
+	}
+
+//=========================Answer Object===========================\\
+
+	/**
+	 * Answer object to help me figure out what people are answering.
+	 */
+	class Answer
+	{
+		/**
+		 * @var string
+		 */
+		private string answer;
+
+		/**
+		 * @var string
+		 */
+		protected string[] falseStatements = { "n", "N", "no", "No", "not", "Not", "false", "False", "negative", "Negative", "lying", "lied", "lies", "na", "Na", "nah", "Nah", "naw", "Naw", "nu", "Nu" +
+			""};
+
+				/**
+		 * @var string
+		 */
+		protected string[] trueStatements = {"y", "Y", "yes", "Yes", "Sure", "sure", "yeah", "Yeah", "true", "True", "yep", "Yep", "yea", "Yea", "dope", "Dope", "zippa zappa zoobies i like boobies9"};
+
+		/**
+		 * The constructor.
+		 * 
+		 * @param string theAnswer
+		 */
+		public Answer(string theAnswer)
+		{
+			answer = theAnswer;
+		}
+
+		/**
+		 * Find out if the answer is any of our known versions of NO.
+		 * 
+		 * @return bool
+		 */
+		public bool isNo()
+		{
+			foreach (string x in falseStatements){
+				if (x.Equals (answer)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+		
+		/**
+		 * Find out if the answer is any of our known versions of YES.
+		 * 
+		 * @return bool
+		 */
+		public bool isYes()
+		{
+			foreach (string x in trueStatements){
+				if (x.Equals (answer)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/**
+		 * Find out if the answer is neither YES or NO.
+		 * 
+		 * @return bool
+		 */
+		public bool isUnknown()
+		{
+			if (!this.isYes () && !this.isNo ()) {
+				return true;
+			}
+			return false;
+		}
+	}
+	//=================================================================\\
+}

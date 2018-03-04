d6377c669dde56b8b77041dc3fce99a759aa1602
Phineas Carberry <phineascarb@gmail.com>
2018-03-04 09:10:59 -0600

    These changes were cool lol

--- TAKB/Program.cs
+++ TAKB/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 
 /*phins cheat sheat
  one = means assign, two == means check if equal to each other
@@ -7,59 +8,97 @@
 namespace TAKB {
 	
 	class MainClass {
-		
 		public static void Main (string[] args) {
-
 //=========================INT Block============================\\
 
 			//Multiplication Integers 
 			int num01;
 			int num02;
-			int num03;
-
-//=========================STRING Block==========================\\
-
+//=========================DOUBLE Block==========================\\
+			//Double dev01;
+			//Double dev02;
 //=========================Stuff n things========================\\
 
-
+		/**
+		 * Beginning Dialogue stage 1
+		 */
 			Console.WriteLine ("I'm the all knowing-ish bot\npress space to begin your journey");
-			Console.ReadKey ();
 
-			Console.WriteLine ("\nLets exploire my mathmatical capabilities");
-			Console.Write ("First some good ol multiplying (gimme a number):");
+			while (true) { 
+				ConsoleKeyInfo info = Console.ReadKey ();
+				KeyPress keyPress = new KeyPress (info.Key);
 
-			num01 = Convert.ToInt32 (Console.ReadLine());
-
-			Console.Write ("Well I need a second one dummy: ");
-
-			num02 = Convert.ToInt32 (Console.ReadLine());
-			num03 = num01 * num02;
-
-			Console.WriteLine ("\nYour number is " + num01*num02*num03);
-			Console.Write ("\nbam arn't I so shmart?");
-			Console.Write ("\nYour answer: ");
-
-			//Get the answer from the user as an answer object
-			Answer userReturn = new Answer (Console.ReadLine ()); 
-
-			if (userReturn.isYes ()) {
-				Console.WriteLine ("Division!");
-				Console.ReadKey ();
+				if (keyPress.isSpace()) {
+					break;
+				}
 			} 
-			else if (userReturn.isUnknown()) {
-				Console.WriteLine ("Ugh your answer is not preprogrammed in my database,\nI'm not thaaat smart yet ok gimme some slack\nLets just move on to division!");
-				Console.ReadKey ();
-			}
-			else {
-					Console.WriteLine ("Oh really? I guess ill work on that\nMoving on to division!");
-					Console.ReadKey ();
-				
-			}
 
+				/**
+				 * Beginning Dialogue stage 2
+				 */
+				Console.WriteLine ("\nLets exploire my mathmatical capabilities");
+				Console.Write ("First some good ol multiplying (gimme a number):");
+
+					/**
+			 		 * Converts user input |(Console.ReadLine());| ToInt32 for calculations
+			 		 */
+
+						num01 = Convert.ToInt32 (Console.ReadLine());
+						
+						//Getting second number 
+						Console.Write ("Well I need a second one dummy: ");
+
+						//Converting second number	
+						num02 = Convert.ToInt32 (Console.ReadLine());
+
+						//takes right answer and repeates the multiplying with num03
+						Console.WriteLine ("well, " + num01 + " times " + num02 + " im pretty sure equals " + num01*num02);
+						Console.Write ("\nbam arn't I so shmart?");
+						Console.Write ("\nYour answer: ");
+
+							/**
+			 				 * Gets the answer (class) from the user as userReturn object
+			 				 */
+								Answer userReturn = new Answer (Console.ReadLine ()); 
+
+									//Tests if object userReturn fits the isYes params and acts accordingly
+									if (userReturn.isYes ()) {
+									Console.WriteLine ("Thanks I've worked hard since my creation to learn things to help you!\nMoving on to my devision skills...");
+									} 
+								
+									//Tests if object userReturn (the answer) equals the params of isUnknown meaning not a preprogrammed answer/response (so then it must be "gibberish") and acts accordingly
+									else if (userReturn.isUnknown()) {
+									Console.WriteLine ("Ugh your answer is not preprogrammed in my database,\nI'm not thaaat smart yet ok gimme some slack\nLets just move on to division!");
+									}
+								
+									//else if its not fitting the isYes or isUnknown params it must be isNo then acts accordingly
+									else {
+									Console.WriteLine ("You know what I, the all knowing-ish bot am using fucking advanced\nlearning algarithems too mathmatize and speel everything right!\nAnyway moving on to devision...");
+									}
+				
+									//reads if space and when true clears previous dialogue
+									Console.WriteLine("Press Space to continue: ");
+									while (true) { 
+										ConsoleKeyInfo info = Console.ReadKey ();
+										KeyPress keyPress = new KeyPress (info.Key);
+
+										if (keyPress.isSpace()) {
+										Console.Clear ();
+										break;
+										}
+									} 
+				
+			// Beginning dialogue of devision block
+			Console.WriteLine ("Enter a number");
+
+										
+		//public class bracket
 		}
+
+	//main class bracket
 	}
 
-//=========================Answer Object===========================\\
+//=========================Answer Class===========================\\
 
 	/**
 	 * Answer object to help me figure out what people are answering.
@@ -74,13 +113,12 @@ namespace TAKB {
 		/**
 		 * @var string
 		 */
-		protected string[] falseStatements = { "n", "N", "no", "No", "not", "Not", "false", "False", "negative", "Negative", "lying", "lied", "lies", "na", "Na", "nah", "Nah", "naw", "Naw", "nu", "Nu" +
-			""};
+		protected string[] falseStatements = { "n", "N", "no", "No", "not", "Not", "false", "False", "negative", "Negative", "lying", "lied", "lies", "na", "Na", "nah", "Nah", "naw", "Naw", "nu", "Nu"};
 
 				/**
 		 * @var string
 		 */
-		protected string[] trueStatements = {"y", "Y", "yes", "Yes", "Sure", "sure", "yeah", "Yeah", "true", "True", "yep", "Yep", "yea", "Yea", "dope", "Dope", "zippa zappa zoobies i like boobies9"};
+		protected string[] trueStatements = {"y", "Y", "yes", "Yes", "Sure", "sure", "yeah", "Yeah", "true", "True", "yep", "Yep", "yea", "Yea", "dope", "Dope", "zippa zappa zoobies i like boobies"};
 
 		/**
 		 * The constructor.
@@ -137,5 +175,40 @@ namespace TAKB {
 			return false;
 		}
 	}
-	//=================================================================\\
+
+//===============================KeyPress==========================\\
+	class KeyPress
+	{
+		/**
+		 * @var string
+		 */
+		private ConsoleKey keyPress;
+
+		/**
+		 * The constructor.
+		 * 
+		 * @param string theAnswer
+		 */
+		public KeyPress(ConsoleKey pressedKey)
+		{
+			keyPress = pressedKey;
+		}
+						
+		public bool isSpace()
+		{
+			if (keyPress != ConsoleKey.Spacebar) {
+				Console.Clear ();
+				Console.WriteLine ("I asked you to press spacebar\ntry pressing that instead of " + keyPress);
+				return false;
+			} 
+			return true;
+		}
+
+	}
+
+
 }
+
+
+//===========================KeyPressInt============================\\
+
